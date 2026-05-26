35b92b9f431eadae184cfdf84f07d07c77d1888c
sydomori <sydneyomori@gmail.com>
2026-05-26 03:08:58 +0300

    feat: add base styles and import logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Dashboard from './components/Dashboard/Dashboard'
-import Products from './components/Products/Products'
+import Dashboard from './pages/Dashboard'
+import Products from './pages/Products'
 
 function App() {
   return(
