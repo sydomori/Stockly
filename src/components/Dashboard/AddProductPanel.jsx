1346c1c4315a343dc724f48a090eb7ec71e061d8
sydomori <sydneyomori@gmail.com>
2026-05-26 22:10:52 +0300

    style: add style to product form textfields

--- src/components/Dashboard/AddProductPanel.jsx
+++ src/components/Dashboard/AddProductPanel.jsx
@@ -6,17 +6,31 @@ import Stack from '@mui/material/Stack'
 import Button from '@mui/material/Button'
 
 export default function AddProductPanel() {
+    const inputStyles = {
+        '& .MuiOutlinedInput-root': {
+            bgcolor: 'var(--background)',
+            borderRadius: 3,
+            '& fieldset': { borderColor: 'var(--border)' },
+            '&:hover fieldset': { borderColor: 'var(--border)' },
+            '&.Mui-focused fieldset': { borderColor: 'var(--primary-action)' },
+        },
+        '& .MuiInputLabel-root': { color: 'var(--muted-text)' },
+        '& .MuiInputLabel-root.Mui-focused': { color: 'var(--primary-action)' },
+        '& .MuiOutlinedInput-input': { color: 'var(--muted-text)' },
+    };
     return (
        <Box sx={{color:'var(--text-primary)',bgcolor:'var(--card-surface)',p:3, mt:10, borderRadius:2}} component="form">
            <Stack direction="column" spacing={2}>
-                <Typography variant='h6' mb={2}>Add Product</Typography>
-                <Stack direction="row" spacing={2}>
-                    <TextField label="Product Name" fullWidth margin="small" placeholder='e.g JBL Headphones' sx={{mb:2}} />
-                    <TextField label="Price (KES)" fullWidth margin="small" placeholder='2000' sx={{mb:2}} />
-                    <TextField label="Image URL" fullWidth margin="small" placeholder='https://...' sx={{mb:2}} />
-                    <Button variant="contained" color="primary">Add Product</Button>
+                <Stack>
+                    <Typography variant='h5' mb={2}>Add Product</Typography>
+                    <Typography sx={{color:'var(--muted-text)'}} variant='body2' mb={2}>Fill in the product details to add a new item to your inventory.</Typography>
                 </Stack>
-                
+                <Stack direction="row" spacing={2}>
+                    <TextField sx={inputStyles} label="Product Name" fullWidth size="small" placeholder='e.g JBL Headphones' />
+                    <TextField sx={inputStyles} label="Price (KES)" fullWidth size="small" placeholder='2000' />
+                    <TextField sx={inputStyles} label="Image URL" fullWidth size="small" placeholder='https://...' />
+                    <Button sx={{width:'200px', bgcolor:'var(--primary-action)',color:'var(--background)'}} variant="contained">Add</Button>
+                </Stack>
             </Stack>
        </Box>
     )
