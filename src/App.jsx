8b71dd85526419f66003134ea61b5e63ae682db8
sydomori <sydneyomori@gmail.com>
2026-05-26 03:27:05 +0300

    install material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
-import Products from './pages/Products'
+// import Products from './pages/Products'
 
 function App() {
   return(
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/products" element={<Products />} />
+        {/* <Route path="/products" element={<Products />} /> */}
     </Routes>
     </BrowserRouter>
   )
