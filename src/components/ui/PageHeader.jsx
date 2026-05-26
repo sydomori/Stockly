9d23859c1cab82d8cb00ddad6bd69b423c3e7dc7
sydomori <sydneyomori@gmail.com>
2026-05-27 00:40:58 +0300

    create addproduct button on pageheader

--- src/components/ui/PageHeader.jsx
+++ src/components/ui/PageHeader.jsx
@@ -5,9 +5,10 @@ import FilterListIcon from '@mui/icons-material/FilterList'
 import DownloadIcon from '@mui/icons-material/Download'
 import Stack from '@mui/material/Stack'
 import "../../App.css"
+import addCircle from '../../assets/addCircle.svg'
 
 
-export default function PageHeader({filterOpen, setFilterOpen}){
+export default function PageHeader({filterOpen, setFilterOpen, isAddingProduct, onToggleAdd}){
     return(
         <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 3, position: 'relative', top: '80px'}}>
             <Stack spacing={0.5}>
@@ -22,6 +23,17 @@ export default function PageHeader({filterOpen, setFilterOpen}){
                 <Button startIcon={<FilterListIcon />} variant="text" sx={{color: 'var(--text-primary)', '&:hover': {bgcolor: 'var(--primary-action)',color:'var(--background)'}}} onClick={() => setFilterOpen(!filterOpen)}>Filter</Button>
                 <Button startIcon={<DownloadIcon />} variant="text" sx={{color: 'var(--text-primary)', '&:hover': {bgcolor: 'var(--primary-action)',color:'var(--background)'}}}>Export</Button>
             </Box>
+            <Box>
+                <Button sx={{borderColor: 'var(--border)', bgcolor: 'var(--card-surface)', color: 'var(--text-primary)', '&:hover': {bgcolor: 'var(--primary-action-hover)'}}} variant="outlined" startIcon={<AddIcon />}>
+                    Add Product
+                </Button>
+            </Box>
         </Box>          
+    )
+}
+
+function AddIcon(){
+    return(
+        <img style={{width: 24, height: 24}} src={addCircle} alt="Add" />
     )
 }
