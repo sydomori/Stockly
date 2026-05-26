ed4e3b121edc55e75c875828dc92e04cc744daef
sydomori <sydneyomori@gmail.com>
2026-05-26 20:54:52 +0300

    style: arrange textfeilds in a row using stack

--- src/components/Dashboard/AddProductPanel.jsx
+++ src/components/Dashboard/AddProductPanel.jsx
@@ -6,10 +6,10 @@ import TextField from '@mui/material/TextField'
 export default function AddProductPanel() {
     return (
        <Box sx={{color:'var(--text-primary)',bgcolor:'var(--card-surface)', mb:3, borderRadius:2}} component="form">
-        <Typography variant='h6' mb={2}>Add Product</Typography>
-        <TextField label="Product Name" fullWidth margin="normal" placeholder='e.g JBL Headphones' sx={{mb:2}} />
-        <TextField label="Price (KES)" fullWidth margin="normal" placeholder='2000' sx={{mb:2}} />
-        <TextField label="Image URL" fullWidth margin="normal" placeholder='https://...' sx={{mb:2}} />
+            <Typography variant='h6' mb={2}>Add Product</Typography>
+            <TextField label="Product Name" fullWidth margin="normal" placeholder='e.g JBL Headphones' sx={{mb:2}} />
+            <TextField label="Price (KES)" fullWidth margin="normal" placeholder='2000' sx={{mb:2}} />
+            <TextField label="Image URL" fullWidth margin="normal" placeholder='https://...' sx={{mb:2}} />
        </Box>
     )
 }
