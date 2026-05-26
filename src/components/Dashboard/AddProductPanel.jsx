c144045c3e699fcc11827841721027ec2597a371
sydomori <sydneyomori@gmail.com>
2026-05-26 20:55:10 +0300

    add stack

--- src/components/Dashboard/AddProductPanel.jsx
+++ src/components/Dashboard/AddProductPanel.jsx
@@ -2,14 +2,18 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import '../../App.css'
 import TextField from '@mui/material/TextField'
+import Stack from '@mui/material/Stack'
 
 export default function AddProductPanel() {
     return (
        <Box sx={{color:'var(--text-primary)',bgcolor:'var(--card-surface)', mb:3, borderRadius:2}} component="form">
             <Typography variant='h6' mb={2}>Add Product</Typography>
-            <TextField label="Product Name" fullWidth margin="normal" placeholder='e.g JBL Headphones' sx={{mb:2}} />
-            <TextField label="Price (KES)" fullWidth margin="normal" placeholder='2000' sx={{mb:2}} />
-            <TextField label="Image URL" fullWidth margin="normal" placeholder='https://...' sx={{mb:2}} />
+            <Stack direction="row" spacing={2}>
+                <TextField label="Product Name" fullWidth margin="normal" placeholder='e.g JBL Headphones' sx={{mb:2}} />
+                <TextField label="Price (KES)" fullWidth margin="normal" placeholder='2000' sx={{mb:2}} />
+                <TextField label="Image URL" fullWidth margin="normal" placeholder='https://...' sx={{mb:2}} />
+            </Stack>
+
        </Box>
     )
 }
