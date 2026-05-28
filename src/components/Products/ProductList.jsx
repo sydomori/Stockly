49a67c989ad76da40c28c805f9a2b1b4c544d975
sydomori <sydneyomori@gmail.com>
2026-05-28 13:52:05 +0300

    fx: fix contaner element overlapping issue

--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -3,7 +3,7 @@ import ProductCard from './ProductCard';
 
 export default function ProductList({products}) {
     return (
-        <Grid sx={{mt:15}} container spacing={2}>
+        <Grid sx={{mt:2}} container spacing={2}>
             {products.map((product) => (
                 <Grid sx={{pb:2}} size={{xs: 12, sm: 6, md: 4, lg: 3}} key={product.id}>
                     <ProductCard product={product} />
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,17 +9,22 @@ import Select from '@mui/material/Select'
 import MenuItem from '@mui/material/MenuItem'
 import AddProductPanel from '../components/Dashboard/AddProductPanel'
 import ProductList from '../components/Products/ProductList'
+import ToggleButton from '@mui/material/ToggleButton'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
+import GridViewIcon from '@mui/icons-material/GridView'
+import ListIcon from '@mui/icons-material/List'
 
 export default function Dashboard({products, addProduct}) {
     const [open, setOpen] = useState(false)
     const [isAddingProduct, setIsAddingProduct] = useState(false)
+    const [view, setView] = useState('grid')
 
     return (
         <>
             <NavBar />
-            <Container>
+            <Container sx={{mt: 1, pb: 2}}>
                 <PageHeader isAddingProduct={isAddingProduct} onToggleAdd={()=> setIsAddingProduct(!isAddingProduct)} filterOpen={open} setFilterOpen={setOpen} />
-                <Collapse in={open} sx={{mt: -5.0, ml:100}}>
+                <Collapse in={open}>
                     <Box sx={{display: 'flex', gap: 2, mt: 2, p: 2, bgcolor: 'var(--primary-action)', borderRadius: 1, width: '300px'}}>
                         <Select size="small" defaultValue="all" sx={{width: '200px'}}>
                             <MenuItem value="all">All</MenuItem>
@@ -41,6 +46,10 @@ export default function Dashboard({products, addProduct}) {
                 <Collapse in={isAddingProduct}>
                     <AddProductPanel onCancel={() => setIsAddingProduct(false)} onAddProduct={addProduct}/>
                 </Collapse>
+                <ToggleButtonGroup sx={{mt:9, bgcolor: 'var(--card-surface)', borderRadius: 1}} value={view} exclusive onChange={(e, newValue) => newValue && setView(newValue)} size="small">
+                    <ToggleButton sx={{color: 'var(--text-primary)'}} value='grid'><GridViewIcon  fontSize='small'/></ToggleButton>
+                    <ToggleButton sx={{color: 'var(--text-primary)'}} value='list'><ListIcon fontSize='small' /></ToggleButton>
+                </ToggleButtonGroup>
                 <ProductList products={products} />
             </Container>
             
