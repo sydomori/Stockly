56757c60ac6a58592b8a8393d2a6916e5d09284f
sydomori <sydneyomori@gmail.com>
2026-07-30 16:07:00 +0300

    feat: add toggle view button to Products component

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
@@ -7,10 +7,15 @@ import ProductList from '../components/Products/ProductList'
 import PageHeader from '../components/ui/PageHeader'
 import Collapse from '@mui/material/Collapse'
 import AddProductPanel from '../components/Dashboard/AddProductPanel'
-import { useState } from 'react'
+import ToggleButton from '@mui/material/ToggleButton'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
+import GridViewIcon from '@mui/icons-material/GridView'
+import ListIcon from '@mui/icons-material/List'
+import ProductTable from '../components/Products/productTable'
 
 export default function Products({products,addProduct}) {
     const [isAddingProduct, setIsAddingProduct] = useState(false)
+    const [view, setView] = useState('grid')
 
     return (
         <Container>
@@ -30,7 +35,13 @@ export default function Products({products,addProduct}) {
                 <Typography sx={{color:'#ffffff'}} variant="h4" fontWeight="bold">
                     Your Products
                 </Typography>
-                <ProductList products={products} />
+                <ToggleButtonGroup sx={{mt:2, bgcolor: 'var(--card-surface)', borderRadius: 1}} value={view} exclusive onChange={(e, newValue) => newValue && setView(newValue)} size="small">
+                    <ToggleButton sx={{color: 'var(--text-primary)'}} value='grid'><GridViewIcon fontSize='small'/></ToggleButton>
+                    <ToggleButton sx={{color: 'var(--text-primary)'}} value='list'><ListIcon fontSize='small' /></ToggleButton>
+                </ToggleButtonGroup>
+                {
+                    view === 'grid' ? <ProductList products={products} /> : <ProductTable products={products} />
+                }
              </Box>
             </Container>
         </Container>
