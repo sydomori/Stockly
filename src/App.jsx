3ee403149f4060a93779cda9f23298a821aec04c
sydomori <sydneyomori@gmail.com>
2026-07-30 19:50:11 +0300

    feat: "update App.jsx with new routes" && "create getstockstatus function "

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import SetPassword from './pages/setPassword'
+import ProtectedRoute from './components/login/protectedRoute'
 import Dashboard from './pages/Dashboard'
 import Products from './pages/Products'
+import { AuthProvider } from './context/AuthContext'
 
 function App(){
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/products" element={<Products />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+           <Route path="/login" element={<Login />} />
+          <Route path="/set-password" element={<SetPassword />} />
+          <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/products" element={<ProtectedRoute><Products /></ProtectedRoute>} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
