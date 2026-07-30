58766837cdf2ad558fdfe89a399c383d9c0e844c
sydomori <sydneyomori@gmail.com>
2026-07-31 02:02:32 +0300

    feat: create get all user activity for admin function

--- src/api/activityLog.js
+++ src/api/activityLog.js
@@ -3,3 +3,7 @@ import request from './client';
 export function getMyActivity() {
   return request('/my-activity');
 }
+
+export function getAllActivity() {
+  return request('/admin/activity-log');
+}
