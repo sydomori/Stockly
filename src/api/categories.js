ac03958704855e93e214ef06baecfcff948a321b
sydomori <sydneyomori@gmail.com>
2026-07-31 01:57:37 +0300

    feat: create additional categories crud functions

--- src/api/categories.js
+++ src/api/categories.js
@@ -3,3 +3,13 @@ import request from './client';
 export function getCategories() {
     return request('/categories');
 }
+
+export function createCategory(data) {
+  return request('/categories', { method: 'POST', body: JSON.stringify(data) });
+}
+export function updateCategory(id, data) {
+  return request(`/categories/${id}`, { method: 'PUT', body: JSON.stringify(data) });
+}
+export function deleteCategory(id) {
+  return request(`/categories/${id}`, { method: 'DELETE' });
+}
